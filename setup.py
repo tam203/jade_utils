a94bfa9ef7de223e97b22176303199db3fd049ee
Rachel Prudden <rachel.prudden@informaticslab.co.uk>
2016-10-03 16:55:30 +0100

    regex

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['s3fs', 're', 'os'],
+    install_requires=['s3fs', 'regex', 'os'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
