4f8da88c1172d553b81bdc38e3c61c986b92cf58
Rachel Prudden <rachel.prudden@informaticslab.co.uk>
2016-10-04 10:43:25 +0100

    asn_utils

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='asnu',
+    name='asn_utils',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
@@ -68,7 +68,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=['asnu'],
+    packages=['asn_utils'],
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
