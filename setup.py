699c601cd95f08121ba59d926d89e96646d69673
Theo McCaie <theo.mccaie@informaticslab.co.uk>
2017-02-01 16:42:50 +0000

    missing os.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 from codecs import open
 import os
 
-here = os.path.abspath(path.dirname(__file__))
+here = os.path.abspath(os.path.dirname(__file__))
 
 # Get the long description from the README file
 with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
