94be3e372eb9f1767558934217bca04a05d5959b
Alex Hilson <alexhilsonwork@googlemail.com>
2017-02-01 15:46:23 +0000

    Add docstrings and pep8-ify Loader class

--- jade_utils/data_tools/loader.py
+++ jade_utils/data_tools/loader.py
@@ -2,24 +2,35 @@ import s3fs
 import re
 import os
 
+
 class Loader:
+    """Class to discover data files."""
     def __init__(self):
         self.local_path = '/usr/local/share/notebooks/data'
         self.fs = s3fs.S3FileSystem()
-        
-    def list_buckets(self):
+
+    def list_datasets(self):
+        """List available datasets."""
         return os.listdir(self.local_path)
-    
-    def list_files(self, dirpath):
+
+    def list_files(self, dataset):
+        """
+        List files from specific dataset.
+
+        Args:
+
+        * dataset:
+            Name of dataset to search for.
+        """
         return [self.local_path + '/' + path for path in self._list_files(dirpath)]
-    
+
     def _list_files(self, dirpath):
         if len(self.fs.ls(dirpath)) == 0:
             return []
         elif self.fs.ls(dirpath, True)[0]['StorageClass'] == 'DIRECTORY':
             # recurse
             child_lists = [self._list_files(child) for child in self.fs.ls(dirpath)]
-            return [f for c in child_lists for f in c] # iterate through all child directories
+            return [f for c in child_lists for f in c]  # iterate through all child directories
         else:
             files = self.fs.glob(dirpath+'/*')
             return files
