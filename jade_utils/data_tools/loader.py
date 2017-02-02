94c917e572b16c66754df0acc307852f74527ec0
Theo McCaie <theo.mccaie@informaticslab.co.uk>
2017-02-02 09:46:57 +0000

    Fix the data loader

--- jade_utils/data_tools/loader.py
+++ jade_utils/data_tools/loader.py
@@ -1,13 +1,10 @@
 from boto.s3.connection import S3Connection
-import re
 import os
-
 
 class Loader:
     """Class to discover data files."""
     def __init__(self):
         self.local_path = '/usr/local/share/notebooks/data'
-        self.fs = s3fs.S3FileSystem()
 
     def list_datasets(self):
         """List available datasets."""
@@ -22,10 +19,10 @@ class Loader:
         * dataset:
             Name of dataset to search for.
         """
-        files = self._list_files(dataset)
-        return [self.local_path + '/' + f for f in files]
+        files = self._list_keys(dataset)
+        return [os.path.join(self.local_path, f) for f in files]
 
     def _list_keys(self, bucket):
         conn = S3Connection()
         s3_bucket = conn.get_bucket(bucket)
-        return ['{}/{}'.format(k.bucket.name, k.key) for k in bucket]
+        return ['{}/{}'.format(k.bucket.name, k.key) for k in s3_bucket]
