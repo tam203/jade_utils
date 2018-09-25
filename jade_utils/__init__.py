7d40cd7ffc5e258dac39cc643d2bf60f0f955f8d
Jacob Tomlinson <jacob.tomlinson@informaticslab.co.uk>
2018-09-25 09:09:01 +0100

    Add tests

--- jade_utils/__init__.py
+++ jade_utils/__init__.py
@@ -1,7 +1,4 @@
-__all__ = ["notebook_tools", "iris_tools", "data_tools"]
-from . import notebook_tools
-from . import iris_tools
-from . import data_tools
+__all__ = ["notebook_tools", "iris_tools", "data_tools", 'human']
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
