7b5d6d50fa2e909b4429a9e4130eb7ce90727506
Jacob Tomlinson <jacobtomlinson@users.noreply.github.com>
2018-09-27 11:31:42 +0100

    Support ints

--- jade_utils/iris_tools/__init__.py
+++ jade_utils/iris_tools/__init__.py
@@ -32,11 +32,11 @@ def estimate_cube_size(cube, humanize=True):
     num_points = functools.reduce(operator.mul, cube.shape, 1)
     num_bytes = 0
     dtype = cube[(0, ) * len(cube.shape)].data.dtype
-    if dtype == 'float16':
+    if dtype == 'float16' or dtype == 'int16':
         num_bytes = 16
-    if dtype == 'float32':
+    if dtype == 'float32' or dtype == 'int32':
         num_bytes = 32
-    if dtype == 'float64':
+    if dtype == 'float64' or dtype == 'int64':
         num_bytes = 64
     total_bytes = (num_points * num_bytes) / 8
     if humanize:
